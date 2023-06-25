438b990343185f2ad731537c45a1affbd82a84a6
turku <turkucakmak@hotmail.com>
2023-06-26 02:37:12 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+   return tutorials.map( function (tutorial){
+    return tutorial.split(" ").map(function (word){
+      return word[0].toUpperCase() + word.slice(1);
+    })
+    .join(' ');
+  }) 
 }
